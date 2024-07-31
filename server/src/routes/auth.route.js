c2a7cd166b267a4bb538bfb72135b94e867d3b20
Vitalii Nezdoiminoha <vitalii.nezdojminoga@gmail.com>
2024-07-31 23:18:08 +0300

    backend fixes

--- server/src/routes/auth.route.js
+++ server/src/routes/auth.route.js
@@ -23,28 +23,12 @@ authRouter.get('/activation/:activationToken', catchError(activate));
 authRouter.post('/login', catchError(login));
 authRouter.get('/refresh', catchError(refresh));
 authRouter.post('/logout', catchError(logout));
-authRouter.post(
-  '/changePassword',
-  catchError(authMiddleware),
-  catchError(resetPassword),
-);
-authRouter.post(
-  '/changeAuthPassword',
-  catchError(authMiddleware),
-  catchError(changeAuthPass),
-);
+authRouter.post('/changePassword', catchError(resetPassword));
+authRouter.post('/changeAuthPassword', catchError(changeAuthPass));
 authRouter.post('/reset', catchError(reset));
 authRouter.get('/reset/:resetToken', catchError(resetChecker));
-authRouter.patch(
-  '/update',
-  catchError(authMiddleware),
-  catchError(updateUserName),
-);
-authRouter.patch(
-  '/confirmChangeEmail',
-  catchError(authMiddleware),
-  catchError(changeEmail),
-);
+authRouter.patch('/update', catchError(updateUserName));
+authRouter.patch('/confirmChangeEmail', catchError(changeEmail));
 
 module.exports = {
   authRouter,
