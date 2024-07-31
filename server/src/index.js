0c02f9a4ddc3a5ae105ffe927d615ce000ad34c5
Vitalii Nezdoiminoha <vitalii.nezdojminoga@gmail.com>
2024-07-31 15:19:32 +0300

    fixes

--- server/src/index.js
+++ server/src/index.js
@@ -25,19 +25,6 @@ app.use(
   }),
 );
 
-app.use((req, res, next) => {
-  res.header(
-    'Access-Control-Allow-Origin',
-    'https://fullstack-auth-app-gold.vercel.app',
-  );
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept',
-  );
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  next();
-});
-
 app.use(express.json());
 app.use(cookieParser());
 
