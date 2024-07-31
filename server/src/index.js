faf26176cbe9c72fc6006d47162ec85edeac432e
Vitalii Nezdoiminoha <vitalii.nezdojminoga@gmail.com>
2024-07-31 15:58:55 +0300

    added cors rules

--- server/src/index.js
+++ server/src/index.js
@@ -18,12 +18,16 @@ const { errorMiddleware } = require('./middlewares/errorMiddleware');
 
 const PORT = process.env.PORT;
 
-app.use(
-  cors({
-    origin: 'https://fullstack-auth-app-gold.vercel.app',
-    credentials: true,
-  }),
-);
+app.use(function (req, res, next) {
+  //Enabling CORS
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization',
+  );
+  next();
+});
 
 app.use(express.json());
 app.use(cookieParser());
