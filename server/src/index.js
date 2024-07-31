37864bff7ba191f9e7badad7c7a482aa920f2ad4
Vitalii Nezdoiminoha <vitalii.nezdojminoga@gmail.com>
2024-07-31 15:08:52 +0300

    fixes

--- server/src/index.js
+++ server/src/index.js
@@ -31,10 +31,6 @@ app.use(cookieParser());
 app.use(authRouter);
 app.use('/users', userRouter);
 
-app.use((req, res) => {
-  res.status(404).send('The page is not found');
-});
-
 app.use(errorMiddleware);
 
 app.listen(PORT, () => {
