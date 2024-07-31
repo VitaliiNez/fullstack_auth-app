82a09152d9bbba174104455af1a64cfe49ab27e4
Vitalii Nezdoiminoha <vitalii.nezdojminoga@gmail.com>
2024-07-31 22:31:50 +0300

    added client env

--- server/src/index.js
+++ server/src/index.js
@@ -18,16 +18,12 @@ const { errorMiddleware } = require('./middlewares/errorMiddleware');
 
 const PORT = process.env.PORT;
 
-app.use(function (req, res, next) {
-  //Enabling CORS
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization',
-  );
-  next();
-});
+app.use(
+  cors({
+    origin: 'https://fullstack-auth-app-gold.vercel.app',
+    credentials: true,
+  }),
+);
 
 app.use(express.json());
 app.use(cookieParser());
